03bdd3b63d9a46e9feacf16f79ab403040796524
Thabiso Kama <218017421@mycput.ac.za>
2025-05-07 14:23:23 +0200

    yessir

--- utils/lyrics_fetcher.py
+++ utils/lyrics_fetcher.py
@@ -1,6 +1,29 @@
 import eyed3
 import os
+import requests
+from urllib.parse import quote
 
+GENIUS_API_KEY = 'u7Ij4hLJSPXAuEGWNmvYqLoBvZJ52z74wYViX55PLpiBW0IgJmMk67ytcXoI3I7bj3d7EehZrXed-t72S7sDww'
+GENIUS_API_URL = 'https://api.genius.com'
+
+def fetch_lyrics_from_genius(song_title, artist_name):
+    song_title = quote(song_title)
+    artist_name = quote(artist_name)
+    search_url = f"{GENIUS_API_URL}/search?q{song_title} {artist_name}"
+    headers = {'Authorisation': f'Bearer {GENIUS_API_KEY}'}
+
+    response = requests.get(search_url, headers=headers)
+    if response.status_code == 200:
+        result = response.json()
+        if result['response']['hits']:
+            song_info = result['response']['hits'][0]['result']
+            song_url = song_info['url']
+            return song_url
+        else:
+            return "Lyrics not found."
+    else:
+        return "Error fetching lyrics"
+    
 def get_lyrics(file_path):
     audio = eyed3.load(file_path)
     if audio.tag and audio.tag.lyrics:
